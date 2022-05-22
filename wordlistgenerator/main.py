e5181dd3011512f7cd7ab906f23f1570c9a2bd0c
Roger Wang <rogerwang2520@gmail.com>
2022-05-22 00:35:37 -0700

    Add word list generator based on most frequent graphemes

--- wordlistgenerator/main.py
+++ wordlistgenerator/main.py
@@ -1,26 +1,66 @@
 import random
-from collections import Counter
+from collections import Counter, defaultdict
 from jsonfilelist import json_file_to_list, list_to_json_file
 
 random.seed(0)
 
-old_word_list_json_file = 'constants/oldwordlist.json'
+old_word_list_json_file = 'constants/allwordlist.json'
 new_word_list_file = 'constants/wordlist.json'
 
-old_valid_guesses_json_file = 'constants/oldvalidGuesses.json'
+old_valid_guesses_json_file = 'constants/allvalidGuesses.json'
 new_valid_guesses_file = 'constants/validGuesses.json'
 
-def get_word_list(old_word_list):
+def get_words_of_length(word_length, word_list):
+    return [word for word in word_list if len(word) == word_length]
+
+def get_grapheme_freq(grapheme_length, word_list, grapheme_overlap=True):
+    grapheme_freq = defaultdict(int)
+    for word in word_list:
+        step = 1 if grapheme_overlap else grapheme_length
+        for i in range(0, len(word) - grapheme_length + 1, step):
+            grapheme = word[i:(i + grapheme_length)]
+            grapheme_freq[grapheme] += 1
+    return grapheme_freq
+
+def get_sorted_grapheme_freq(grapheme_freq):
+    return sorted(grapheme_freq.items(), key=lambda kv: kv[1], reverse=True)
+
+def get_word_list(old_word_list, graphemes, grapheme_overlap=True):
     new_word_list = []
-    # TODO
+    for word in old_word_list:
+        if can_form_with_graphemes(word, graphemes, grapheme_overlap):
+            new_word_list.append(word)
     return new_word_list
 
+def can_form_with_graphemes(word, graphemes, grapheme_overlap=True):
+    step = 1 if grapheme_overlap else grapheme_length
+    for i in range(0, len(word) - grapheme_length + 1, step):
+        grapheme = word[i:(i + grapheme_length)]
+        if grapheme not in graphemes:
+            return False
+    return True
+
+word_length = 8
+grapheme_length = 2
+grapheme_count = 32
+
 old_word_list = json_file_to_list(old_word_list_json_file)
-new_word_list = get_word_list(old_word_list)
-random.shuffle(new_word_list)
+old_word_list = get_words_of_length(word_length, old_word_list)
+
+grapheme_freq = get_grapheme_freq(grapheme_length, old_word_list, grapheme_overlap=False)
+sorted_grapheme_freq = get_sorted_grapheme_freq(grapheme_freq)
+top_graphemes = [grapheme_kv[0] for grapheme_kv in sorted_grapheme_freq[:grapheme_count]]
+# print(top_graphemes)
+
+new_word_list = get_word_list(old_word_list, top_graphemes, grapheme_overlap=False)
+# random.shuffle(new_word_list)
 list_to_json_file(new_word_list, new_word_list_file)
 
 old_valid_guesses = json_file_to_list(old_valid_guesses_json_file)
-new_valid_guesses = get_word_list(old_valid_guesses)
+old_valid_guesses = get_words_of_length(word_length, old_valid_guesses)
+new_valid_guesses = get_word_list(old_valid_guesses, top_graphemes, grapheme_overlap=False)
 new_valid_guesses.sort()
 list_to_json_file(new_valid_guesses, new_valid_guesses_file)
+
+
+# Replace "*****" strings in json file by removing `  "\*+",\n` regex matches
